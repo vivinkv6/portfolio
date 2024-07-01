e5272c8daadc6ebca9dd70c71460e1c38152532a
vivinkv6 <vivinkv0@gmail.com>
2024-07-01 20:28:40 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   description:
     "Passionate about creating robust and scalable web applications. Experienced in both front-end and back-end technologies, with a knack for problem-solving and delivering high-quality code.",
   keywords: [
+    'vivinkv',
+     'vivin',
     "Sri C Achuthamenon Govt College Thrissur",
     "Front-end development",
     "MERN stack",
