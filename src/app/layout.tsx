e83eeea8ce5f9fa4550702f8de908652048ead25
vivinkv6 <vivinkv0@gmail.com>
2024-07-12 07:55:32 +0530

    add metadata to the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,6 @@ import Nav from "@/app/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Vivin KV",
-  description:
-    "Passionate about creating robust and scalable web applications. Experienced in both front-end and back-end technologies, with a knack for problem-solving and delivering high-quality code.",
-  keywords: ["vivin-kv", "mern", "next", "react-native"],
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-
 import React from "react";
 import HeroSection from "@/app/components/HeroSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vivin KV",
+  description:
+    "Passionate about creating robust and scalable web applications. Experienced in both front-end and back-end technologies, with a knack for problem-solving and delivering high-quality code.",
+  keywords: ["vivin-kv", "full-stack-developer", "vivinkv"],
+};
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-950 text-gray-200">
-     <HeroSection/>
+      <HeroSection />
     </div>
   );
 };
