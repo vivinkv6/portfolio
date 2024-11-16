4fdcf4c770524d679ef56343b6747a588a604aec
VIVIN KV <102971812+vivinkv6@users.noreply.github.com>
2024-11-16 17:01:22 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,37 +8,37 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const blog: BloggerResponse = await blogList.json();
 
   return [
-    {
-      url: `${process.env.URL}`,
-      priority: 1,
-    },
-    {
-      url: `${process.env.URL}/about`,
-      priority: 0.9,
-    },
-    {
-      url: `${process.env.URL}/education`,
-      priority: 0.8,
-    },
-    {
-      url: `${process.env.URL}/experience`,
-      priority: 0.7,
-    },
-    {
-      url: `${process.env.URL}/skills`,
-      priority: 0.6,
-    },
-    {
-      url: `${process.env.URL}/project`,
-      priority: 0.5,
-    },
-    {
-      url: `${process.env.URL}/blogs`,
-      priority: 0.4,
-    },
-    ...blog.items.map((post) => ({
-      url: `${process.env.URL}/blogs/${post.id}`,
-      priority: 0.3,
-    })),
-  ];
+  {
+    url: `${process.env.URL}`,
+    priority: 1, 
+  },
+  {
+    url: `${process.env.URL}/about`,
+    priority: 0.9, 
+  },
+  {
+    url: `${process.env.URL}/education`,
+    priority: 0.8, 
+  },
+  {
+    url: `${process.env.URL}/experience`,
+    priority: 0.8, 
+  },
+  {
+    url: `${process.env.URL}/skills`,
+    priority: 0.6, 
+  },
+  {
+    url: `${process.env.URL}/project`,
+    priority: 0.7,
+  },
+  {
+    url: `${process.env.URL}/blogs`,
+    priority: 0.6,
+  },
+  ...blog.items.map((post) => ({
+    url: `${process.env.URL}/blogs/${post.id}`,
+    priority: 0.4,
+  })),
+]
 }
